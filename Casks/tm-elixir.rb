10939dc45a61365a69145e4e711d338b610fb624
Josh Lubaway <jish@users.noreply.github.com>
2017-08-22 06:54:12 -0700

    Lock Elixir Bundle to a specific version. (#5)
    This allows us to check that the file we expect to download is in fact
    the bundle that we are downloading.

--- Casks/tm-elixir.rb
+++ Casks/tm-elixir.rb
@@ -1,11 +1,11 @@
 cask "tm-elixir" do
-  version :latest
-  sha256 :no_check
+  version "1b4315ffd8e01769c254f5f5dbccc23cc469a6b8"
+  sha256 "6b6277aae39bfb78ea22d99d923adda8f5c242b74aca3433278f5a24d4ea2fa3"
 
-  url "https://github.com/elixir-lang/elixir-tmbundle/archive/master.tar.gz"
+  url "https://github.com/elixir-lang/elixir-tmbundle/archive/#{version}.tar.gz"
 
   name "A TextMate Bundle for the Elixir programming language."
   homepage "https://github.com/elixir-lang/elixir-tmbundle"
 
-  tmbundle "elixir-tmbundle-master", target: "Elixir.tmbundle"
+  tmbundle "elixir-tmbundle-#{version}", target: "Elixir.tmbundle"
 end
