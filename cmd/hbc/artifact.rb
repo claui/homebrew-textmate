0cd6e78b6060e3834509f8ed760304e1b4e87f9f
Claudia <claui@users.noreply.github.com>
2017-09-18 07:50:43 +0200

    See https://github.com/Homebrew/brew/pull/3141

--- cmd/hbc/artifact.rb
+++ cmd/hbc/artifact.rb
@@ -5,9 +5,9 @@ module Hbc
   module Artifact
     original_for_cask_method = method(:for_cask)
 
-    define_singleton_method(:for_cask) do |cask, options = {}|
-      original_for_cask_method.(cask, options).tap do |types|        
-        types << Tmbundle.new(cask, options) if Tmbundle.me?(cask)
+    define_singleton_method(:for_cask) do |cask|
+      original_for_cask_method.(cask).tap do |classes|
+        classes << Tmbundle.new(cask)
       end
     end
   end
