8757491f367b70669f35a2e46c9184723c60fa43
Josh Lubaway <josh.lubaway@gmail.com>
2017-10-22 14:14:30 -0700

    Add JavaScript-Mocha

---
+++ Casks/tm-javascript-mocha.rb
@@ -0,0 +1,11 @@
+cask "tm-javascript-mocha" do
+  version "141614f34614162d55bb7eebbe46fca0f894a49f"
+  sha256 "6b6277aae39bfb78ea22d99d923adda8f5c242b74aca3433278f5a24d4ea2fa3"
+
+  url "https://github.com/elixir-lang/elixir-tmbundle/archive/#{version}.tar.gz"
+
+  name "Mocha test bundle for TextMate."
+  homepage "https://github.com/fiveisprime/mocha.tmbundle"
+
+  tmbundle "mocha-tmbundle-#{version}", target: "JavaScript-Mocha.tmbundle"
+end
