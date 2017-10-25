b193164427849d3edd19998370450f3ed8bf2340
Josh Lubaway <josh.lubaway@gmail.com>
2017-10-24 21:56:59 -0700

    Update shasum for JavaScript-Mocha

--- Casks/tm-javascript-mocha.rb
+++ Casks/tm-javascript-mocha.rb
@@ -1,6 +1,6 @@
 cask "tm-javascript-mocha" do
   version "141614f34614162d55bb7eebbe46fca0f894a49f"
-  sha256 "6b6277aae39bfb78ea22d99d923adda8f5c242b74aca3433278f5a24d4ea2fa3"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 
   url "https://github.com/fiveisprime/mocha/archive/#{version}.tar.gz"
 
